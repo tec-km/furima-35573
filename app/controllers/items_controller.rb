3e0da4a92bd8e8849bdbf2931df2d8c35cfaf813
tec-km <kamiyama2021@gmail.com>
2021-05-25 11:35:06 +0900

    アクティブハッシュの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,20 +2,28 @@ class ItemsController < ApplicationController
   # before_action :authenticate_user!
   
   
-  
   def index
     
   end
 
+  def edit
+    
+  end
+
+  def new
+    
+  end
+
+  def show
+    
+  end
 
 
+  private
 
-
- 
-
-
-
-
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
 
 
