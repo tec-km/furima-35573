773a1866e00129f79725fefdcf438f552b609fcb
tec-km <kamiyama2021@gmail.com>
2021-05-28 12:07:15 +0900

    商品編集機能再提出1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,55 +1,31 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
-  
+  before_action :set_item,      only: [:show, :edit, :update]
+  before_action :move_to_index, only:[:edit, :update]
+
+
   def index
     @items= Item.all.order("created_at DESC")
   end
 
-  def edit
-    @item = Item.find(params[:id])
-
-
-    if current_user.id != @item.user_id 
-      redirect_to root_path
-    end
-
-
-    
+  def edit   
   end
 
   def update
-    @item = Item.find(params[:id])
-    
-    if current_user.id != @item.user_id 
-      redirect_to root_path
-    end
-   
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
     render :edit
     end
 
-
-    
-
   end
-
-  def item_params
-    params.require(:item).permit(:image, :title, :description, :category_id, :shipping_id, :day_id, :price, :user_id)
-  end
-
-
 
   def new
     @item = Item.new
   end
 
   def show  
-    @item = Item.find(params[:id])
   end
-
 
   def create
     @item = Item.new(item_params)
@@ -68,6 +44,15 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :title, :description, :category_id, :condition_id, :shipping_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
   end
 
+def  set_item
+  @item = Item.find(params[:id])
+end
+
+def move_to_index
+  if current_user.id != @item.user_id 
+    redirect_to root_path
+  end
+end
 
 
 end
