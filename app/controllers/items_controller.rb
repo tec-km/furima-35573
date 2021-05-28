233057789c7788e996c1459edf5c29ff5a4d08cc
tec-km <kamiyama2021@gmail.com>
2021-05-28 11:36:09 +0900

    出品したユーザーのみの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,15 +8,32 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+
+
+    if current_user.id != @item.user_id 
+      redirect_to root_path
+    end
+
+
+    
   end
 
   def update
     @item = Item.find(params[:id])
+    
+    if current_user.id != @item.user_id 
+      redirect_to root_path
+    end
+   
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
     render :edit
     end
+
+
+    
+
   end
 
   def item_params
